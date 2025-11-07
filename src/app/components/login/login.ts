4158db0f9c3f31e5ab726a549583cb86b6f47008
Jeanmary Danielys <jeanmarydanielys3@gmail.com>
2025-11-06 23:38:31 -0500

    feat(login): agregar validación de formulario y toggle de contraseña

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
-export class Login {
+export class LoginComponent {
+  loginForm: FormGroup;
+  showPassword = false;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      usuario: ['', Validators.required],
+      contrasena: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    console.log('Login:', this.loginForm.value);
+  }
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+  clearField(fieldName: string) {
+    this.loginForm.get(fieldName)?.setValue('');
+  }
 }
